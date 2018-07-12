0c33a1b283e8e726fbf8a52dcddc5b028a524416
Ewelina Kijanowska <kijanowska@gmail.com>
2018-07-12 17:01:34 +0200

    service worker

---
+++ sw.js
@@ -0,0 +1,57 @@
+
+// Sw implementation credit: https://developers.google.com/web/fundamentals/primers/service-workers/
+
+const CACHE_NAME = 'rest-rev';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/restaurant.html',
+    '/css/',
+    '/img/',
+    '/data/',
+    '/js/'
+];
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                let fetchRequest = event.request.clone();
+
+                return fetch(fetchRequest).then(
+                    function(response) {
+                        // Check if we received a valid response
+                        if(!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+                        let responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(function(cache) {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
+            })
+    );
+});
+
+
